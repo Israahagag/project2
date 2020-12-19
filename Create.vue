e132f24d42c6d367fe6cbae2ce9d27b5144c24d6
Israa Hagag <74570252+Israahagag@users.noreply.github.com>
2020-12-19 15:01:34 +0200

    Add files via upload

---
+++ Create.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="app">
+    <h1>Vue Select</h1>
+
+    <form>
+      <input type="text" v-model="name" placeholder="name" />
+      <select v-model="devision">
+        <option> </option>
+        <option
+          :value="option"
+          v-for="(option, index) in options"
+          :key="index"
+          >{{ option }}</option
+        >
+      </select>
+      <br />
+
+      <select v-model="grade">
+        <option> </option>
+        <option :value="id" v-for="(id, index) in id" :key="index">{{
+          id
+        }}</option>
+      </select>
+      <br />
+    </form>
+    <button type="button" @click="addRow">Submit</button>
+  </div>
+</template>
+
+<script>
+//import vSelect from "vue-select";
+//import Vuelidate from "vuelidate";
+
+export default {
+  name: "Create",
+  components: {
+    //vSelect,
+    // Vuelidate,
+  },
+  data: function() {
+    return {
+      options: ["grad1", "grad2", "grad3", "grad4"],
+      id: [1, 2, 3, 4, 5],
+      selected: "",
+      arr: [],
+      name: "",
+      devision: {},
+      grade: {},
+      class: {
+        name: "",
+        devision: {},
+        grade: {},
+      },
+    };
+  },
+  methods: {
+    addRow() {
+      this.arr.push(this.selected);
+
+      // what to push unto the rows array?
+    },
+    watch: {
+      name(v) {
+        console.log("name", v);
+      },
+      devision(v) {
+        console.log("devision", v);
+      },
+      grade(v) {
+        console.log("grade", v);
+      },
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif;
+  text-rendering: optimizelegibility;
+  -moz-osx-font-smoothing: grayscale;
+  -moz-text-size-adjust: none;
+}
+#app {
+  background-color: #fff;
+}
+h1,
+.muted {
+  color: #2c3e5099;
+}
+
+h1 {
+  font-size: 26px;
+  font-weight: 600;
+}
+
+#app {
+  max-width: 30em;
+  margin: 1em auto;
+}
+button {
+  float: right;
+  border-radius: 5px;
+  color: #fff;
+  width: 20%;
+  background: green;
+}
+select {
+  width: 150px;
+  height: 30px;
+  margin: 10px;
+}
+</style>
---
+++ list.vue
@@ -0,0 +1,47 @@
+<!--<template>
+
+<form id="app" @submit="checkForm" action="/something" method="post">
+  
+  <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+     <li v-for="error in errors">{{ error }}</li>
+    </ul>
+  </p>
+  
+  <p>
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name" v-model="name">
+  </p>
+
+  <p>
+    <label for="age">Age</label>
+    <input type="number" name="age" id="age" v-model="age" min="0">
+  </p>
+
+</form>
+</template>
+
+
+<script>
+export default ({
+    name: "list",
+    components:{},
+    data: {
+    errors:[],
+    name:null,
+    age:null,
+    movie:null
+  },
+  methods:{
+    checkForm:function(e) {
+      if(this.name && this.age) return true;
+      this.errors = [];
+      if(!this.name) this.errors.push("Name required.");
+      if(!this.age) this.errors.push("Age required.");
+      e.preventDefault();
+    }
+  }
+
+})
+</script> -->
