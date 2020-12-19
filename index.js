96252058969e7744eca997605a2432c66d2a8bf9
Israa Hagag <74570252+Israahagag@users.noreply.github.com>
2020-12-19 15:02:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-import { createStore } from "vuex";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Create from "../views/Create.vue";
+import list from "../views/list.vue";
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    components: About
+   
+  },
+  {
+    path: "/create",
+    name: "Create",
+    component: Create,
+  },
+  {
+    path: "/list",
+    name: "list",
+    component: list,
+  },
+  
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
+
+export default router;
